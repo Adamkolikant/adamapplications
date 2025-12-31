27b3eeae9f5217c2ff793c0fc266004094b3c42f
adamkolikant <adam.kolikantnew@gmail.com>
2025-12-31 14:34:36 +0200

    תפריטים

--- app/src/main/java/com/katza/myapp/MainActivity2.java
+++ app/src/main/java/com/katza/myapp/MainActivity2.java
@@ -1,6 +1,12 @@
 package com.katza.myapp;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +26,54 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        registerForContextMenu(findViewById(R.id.textView));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        super.onOptionsItemSelected(item);
+
+        int id = item.getItemId();
+
+        if (id == R.id.activity_main1) {
+            Toast.makeText(this, "activity_main1", Toast.LENGTH_SHORT).show();
+        }
+
+        else if (id == R.id.activity_main2) {
+            Toast.makeText(this, " activity_main2", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu2, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.firstline) {
+            Toast.makeText(this, "You selected first line", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        else if (item.getItemId() == R.id.secondline) {
+            Toast.makeText(this, "You selected second line", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return false;
     }
 }
